7f1803f03a32db6eb22c4be3fafe5012c2ae4dc2
raddclub <clubradd@gmail.com>
2026-05-31 17:15:18 -0700

    fix(flutter): resolve RequestEncoder name conflict with dio
    Dio v5 exports RequestEncoder from options.dart which collides
    with our core/security/request_encoder.dart class.
    Fix: add "hide RequestEncoder" to the Dio import in api_client.dart.

--- raddflix_flutter/lib/core/api/api_client.dart
+++ raddflix_flutter/lib/core/api/api_client.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-import 'package:dio/dio.dart';
+import 'package:dio/dio.dart' hide RequestEncoder;
 import '../constants.dart';
 import '../security/keystore.dart';
 import '../security/app_guard.dart';
