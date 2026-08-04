961d74bc32b62e20cf8d9ed52d5963b373827787
raddclub <clubradd@gmail.com>
2026-08-04 08:52:30 -0700

    Fix CI: add rendering import for ScrollDirection in live_tv_screen.dart

--- raddflix_flutter/lib/screens/live_tv_screen.dart
+++ raddflix_flutter/lib/screens/live_tv_screen.dart
@@ -12,6 +12,7 @@
 
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart' show ScrollDirection;
 import 'package:flutter/services.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter_animate/flutter_animate.dart';
